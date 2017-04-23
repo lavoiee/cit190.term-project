039d9afc277fb97fe3236001e30e767bd364c3d8
lavoiee <ericjohnlavoie@gmail.com>
2017-04-22 20:49:35 -0400

    Set up localStorage for the registration form and tested it. It works.

--- assets/utilities/Utility/js/script.js
+++ assets/utilities/Utility/js/script.js
@@ -1,9 +1,23 @@
 
+
+
 $(function(){
   $('li#submit').on('click', function(){
 
   });
   $('li#finalize').on('click', function(){
+
+  });
+  $('#register #register-btn').on('click', function(){
+    var iUsername = $('#register #username').val();
+    var iEmail = $('#register #email').val();
+    var iPass = $('#register #password').val();
+    localStorage.setItem('username', iUsername);
+    localStorage.setItem('email', iEmail);
+    localStorage.setItem('password', iPass);
+    
+  });
+  $('#login #login-btn').on('click', function(){
 
   });
 });
