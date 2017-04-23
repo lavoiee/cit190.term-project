d197e09d79cd88a2ab2afe3984907c47822a4989
lavoiee <ericjohnlavoie@gmail.com>
2017-04-23 17:24:48 -0400

    Had to change the way I use the script for the quiz.html page. The last solution that I said worked, did not. It fired for both pages even though I used the body#quiz selector. So I ran a test and created a separate js file named quiz-script.js and tested it to see it it could access the localStorage for the other page. This works.

--- assets/utilities/Utility/js/script.js
+++ assets/utilities/Utility/js/script.js
@@ -152,8 +152,3 @@ $(function(){
     localStorage.removeItem('authenticated');
   });
 });
-
-// JavaScript for Quiz page.
-$('#quiz').on('ready', function(){
-  alert('The quiz page has loaded.');
-});
