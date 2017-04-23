81432ee0d25f5e17d70d0c38f60db503030a30ff
lavoiee <ericjohnlavoie@gmail.com>
2017-04-23 12:36:51 -0400

    Added some comments in th js file for the Quiz Utility.

--- assets/utilities/Utility/js/script.js
+++ assets/utilities/Utility/js/script.js
@@ -14,16 +14,19 @@ var correstAnswer = false;
 
 $(function(){
   $('li#submit').on('click', function(){
+    // Test to see if user has created an account or logged in.
     if (localStorage.authenticated) {
+      // If so, pull all of the information from the form.
       questionNumber = $('#questionNumber option:selected').text();
       numberOfAnswers = $('#numberOfAnswers option:selected').text();
       answerNumber = $('#answerNumber option:selected').text();
       correctAnswer = $('#correctAnswer option:selected').text();
       question = $('#question').val();
       answer = $('#answer').val();
-      
+
     }
     else {
+      // In not, feedback to user.
       alert('You need to be authenticated before you can create a quiz.');
     }
 
@@ -32,10 +35,15 @@ $(function(){
   $('li#finalize').on('click', function(){
 
   });
+
+  // Event Listener for reset button. Reset form.
   $('li#reset').on('click', function(){
     $('#question').val('');
     $('#answer').val('');
   });
+
+  // Event Listener for registration. Get information from user,
+  // store in local storage.
   $('#register #register-btn').on('click', function(){
     var iUsername = $('#register #username').val();
     var iEmail = $('#register #email').val();
@@ -44,6 +52,9 @@ $(function(){
     localStorage.setItem('email', iEmail);
     localStorage.setItem('password', iPass);
   });
+
+  // Event Lisener for Login. Check input against local storage.
+  // Authenticate if match.
   $('#login #login-btn').on('click', function(){
     var iUsername = $('#login #username').val();
     if (iUsername === localStorage.username) {
