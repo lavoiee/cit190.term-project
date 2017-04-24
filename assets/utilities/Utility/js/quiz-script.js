da520497ed3d00adc37adfa9ef9ece759ea1247c
lavoiee <ericjohnlavoie@gmail.com>
2017-04-23 21:02:57 -0400

    Created a new Quiz object in quiz-script.js to hold the JSON representation of the Quiz object from index.html. I had to do this to enable access to the members of the Quiz object. I wasn't able to access the members while it was in JSON format in localStorage. It works now. I just tested it with alert(quiz.q1Array).

--- assets/utilities/Utility/js/quiz-script.js
+++ assets/utilities/Utility/js/quiz-script.js
@@ -1,6 +1,23 @@
 // Hide the quiz-container div until the quiz has bee set up.
 $('#quiz-container').hide();
 
+// Object to hold the JSON quiz information that
+// was stored in localStorage from the script.js file.
+function Quiz(){
+  this.q1Array = [];
+  this.q2Array = [];
+  this.q3Array = [];
+  this.q4Array = [];
+  this.q5Array = [];
+  this.q6Array = [];
+
+  this.checkAnswers = function(){
+
+  };
+}
+
+var quiz = new Quiz();
+
 $(function(){
 
   if (localStorage.getItem('quizReady') === null) {
@@ -8,5 +25,7 @@ $(function(){
   }
   else {
     $('#quiz-container').show();
+    quiz = JSON.parse(localStorage.quiz);
+    
   }
 });
